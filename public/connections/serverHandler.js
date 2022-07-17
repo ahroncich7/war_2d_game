b0f1f82c93bf2a7e7a01c98709e430001c5f8dd7
ahroncich7 <alehroncich@gmail.com>
2022-07-17 16:29:47 -0300

    Add the folder to specify the functions for handle server orders

---
+++ public/connections/serverHandler.js
@@ -0,0 +1,33 @@
+serverHandler = {
+
+    socket: io.connect(),
+
+    connectToServer() {
+        this.socket.on("mensaje", function(data) {
+
+        })
+    },
+
+
+    sendNewUnit(unit) {
+        this.socket.emit("getNewUnit", unit)
+    },
+
+
+    listenNewUnit() {
+        this.socket.on("newUnit", (unit) => {
+            gameHandler.setUnit(unit.type, unit.owner, unit.position)
+        })
+    },
+
+
+    sendDestroyUnit(unit) {
+        this.socket.emit("destroyUnit", unit)
+    },
+
+    listenDestroyUnit() {
+        this.socket.on("unitDestroyed", (unit) => {
+            gameHandler.destroyUnit(unit)
+        })
+    }
+}
