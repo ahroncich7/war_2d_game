63e23c25eb906d75a8165ad7c0feb605f96d689b
ahroncich7 <alehroncich@gmail.com>
2022-07-31 10:42:32 -0300

    Envia acciones al server en lugar de local

--- public/uiHandler.js
+++ public/uiHandler.js
@@ -1,14 +1,20 @@
 var uiHandler = {
+
+    
     deleteUnit() {
         let unit = gameHandler.selectedUnit;
         if (unit && unit.owner == gameHandler.ownerTurn) {
-            gameHandler.deleteObject(gameHandler.selectedUnit);
+            serverHandler.sendDestroyUnitToServer(unit.id);
             gameHandler.unselectAll();
         }
 
     },
 
     addUnit() {
-        socket.emit("getNewUnit", { type: "ground", owner: gameHandler.ownerTurn, position: { x: 0, y: 0 } })
+        serverHandler.sendCreateNewUnitToServer({type:"ground", owner: gameHandler.ownerTurn ,position: gameHandler.playerHome})
     },
 }
+
+document.getElementById("add-unit").addEventListener("click", uiHandler.addUnit)
+document.getElementById("delete-unit").addEventListener("click", uiHandler.deleteUnit) 
+
